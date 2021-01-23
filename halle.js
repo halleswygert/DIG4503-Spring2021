394809903ddc9cc0beef537276f9d390bf8a2d8a
halleswygert <77304766+halleswygert@users.noreply.github.com>
2021-01-23 16:19:07 -0500

    lab 1 work
    working lab 1 files

---
+++ halle.js
@@ -0,0 +1,8 @@
+import Person from "./person.js"; 
+
+class Halle extends Person {
+    constructor(name, favoriteColor){
+        super(name, favoriteColor);
+    }
+}
+export default Halle; 
---
+++ index.js
@@ -0,0 +1,5 @@
+import Halle from "./halle.js";
+
+let me = new Halle('Halle', "Black");
+
+me.speak(); 
