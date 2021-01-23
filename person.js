6834aaf06d1a1e03321bced9a9f5d19a4ae21809
halleswygert <77304766+halleswygert@users.noreply.github.com>
2021-01-23 16:32:27 -0500

    Update person.js

--- person.js
+++ person.js
@@ -4,5 +4,9 @@ class Person {
     constructor(name, favoriteColor) {
         this.name = name;
         this.favoriteColor = favoriteColor;
+    }
     speak() {
        console.log(chalk.hex(this.favoriteColor)(this.name));
+    }
+}
+export default Person; 
