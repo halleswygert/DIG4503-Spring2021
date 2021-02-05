953b8ad721524e0fad02b6b976aefed06757d3c5
halleswygert <77304766+halleswygert@users.noreply.github.com>
2021-02-04 19:06:02 -0500

    Lab 3 Working
    Lab 3 Working

---
+++ lab3/src/App.js
@@ -0,0 +1,8 @@
+import HomePage from './components/HomePage/index.js';
+
+function App() {
+    return (
+      <HomePage firstName="Halle"></HomePage>
+    );
+}
+export default App; 
---
+++ lab3/src/components/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomePage extends React.Component{
+    constructor(props){
+        super (props); 
+    }
+render (){
+    return (
+        <p>{this.props.firstName}</p>
+    );
+}
+}
+export default HomePage; 
