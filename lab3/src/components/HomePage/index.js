136b135a5c121bb491f1202d93ca3661dd8fe355
halleswygert <77304766+halleswygert@users.noreply.github.com>
2021-02-06 20:38:58 -0500

    Update index.js

--- lab3/src/components/HomePage/index.js
+++ lab3/src/components/HomePage/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 class HomePage extends React.Component{
-    constructor(props){
-        super (props); 
-    }
+
 render (){
     return (
         <p>{this.props.firstName}</p>
