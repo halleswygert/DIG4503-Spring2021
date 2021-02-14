4273de94cdc9113985f73ebf574688eaaf1c3af5
halleswygert <77304766+halleswygert@users.noreply.github.com>
2021-02-13 19:20:07 -0500

    lab 4 finishined

---
+++ lab4/src/components/Market.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MarketItem from './MarketItem.js';
+
+class Market extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {items:[]};
+    };
+    render(){
+        return(
+            <div>
+                <h2>Click here to add items!</h2>
+
+                <button onClick = {()=> {
+                    const items = this.state.items;
+                    items.push(<MarketItem count = {items.length}/>);
+                    this.setState({items: items});
+                }}>Press me!</button>
+
+                <div>
+                    {this.state.items.map((count, key) => {
+                         return <p key = {key}>{count}</p>
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Market;
