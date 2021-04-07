1706743f6d1518036a067c2a4e1719c6d495ba4d
halleswygert <77304766+halleswygert@users.noreply.github.com>
2021-04-07 15:14:57 -0400

    Update index.js

--- lab11/index.js
+++ lab11/index.js
@@ -1,10 +1,9 @@
-import Express, { response } from 'express';
+import Express from 'express';
 import CORS from 'cors';
 import Database from './Database.js'
 
 const App= Express();
 const port= 45030; 
-const database = new Database();
 
 App.use(Express.json()); 
 App.use(CORS()); 
@@ -12,28 +11,28 @@ App.use(CORS());
 
 App.put("/books/:ISBN", (req,res) => {
   let newDoc = 
-  database.createOne(request.params.title, 
+  Database.createOne(request.params.title, 
   request.params.author,request.params.description); 
 
   response.json(newDoc); 
 }); 
 
 App.get("/books/:ISBN", (req,res) =>{
-    database.readOne();
+    Database.readOne();
     
 });
 
 App.post("/books/search/:books", (req,res) =>{
-    database.readMany(); 
+    Database.readMany(); 
 
 }); 
 
 App.patch("/books/:ISBN", (req,res) => {
-    database.updateOne();
+    Database.updateOne();
 }); 
 
 App.delete("/books/:ISBN", (req,res) => {
-    database.deleteOne(); 
+   Database.deleteOne(); 
 
 }); 
 
